e59a6e18cd3472802902a7378a7e9dc2561ccdd2
NotAHackusator <amir1peleg@gmail.com>
2022-10-10 20:17:25 +0300

    more style & stuff

--- js.js
+++ js.js
@@ -3,8 +3,8 @@ function validID(id) {
     return !(/[^a-z0-9_]/.test(id));
 }
 function generate() {
-    const modId = get('modid');
-    const blockId = get('blockId');
+    const modId = get('modid').value;
+    const blockId = get('blockId').value;
     // if the name of the mod or block are illegal, alert the user and stop the function
     if (!validID(modId) || !validID(blockId)) {
         alert('Mod ID / Block ID contain illegal characters');
